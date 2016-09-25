dd2336a2165158420f6c676ddf4851fbc3869fb5
Vishwas Anand Bhushan <vishwasanand.cse@gmail.com>
2016-09-25 11:27:02 +0530

    learning

--- Lecture 04/app.js
+++ Lecture 04/app.js
@@ -1,4 +1,19 @@
 (function (){
+'use strict'
+  var app = angular.module("myFirstApp",[]);
+  app.controller('firstController', ControllerFunction);
 
-  
+  function ControllerFunction($scope,$filter){
+    $scope.name="";
+    $scope.length = 0;
+    $scope.displayLength = function (){
+       $scope.length  =   $scope.name.length;
+    };
+console.log($filter);
+    $scope.upper = function(){
+      var toUpper = $filter('uppercase1');
+      $scope.name = toUpper($scope.name);
+    };
+
+  }
 })();
