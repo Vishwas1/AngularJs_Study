0bc5168be7640eb6399b754a21ebde864e653fac
Vishwas Anand Bhushan <vishwasanand.cse@gmail.com>
2016-10-14 07:14:42 +0530

    issue fiix

--- Assignment03 - Copy/app.js
+++ Assignment03 - Copy/app.js
@@ -28,6 +28,7 @@ function NarrowItDownController(MenuSearchService){
     controller.show =false;
     controller.showLoader =true;
     controller.message ="";
+
     if(controller.searchText !== ""){
       var promise  = MenuSearchService.getMatchedMenuItems();
       promise.then(function (response){
@@ -39,9 +40,9 @@ function NarrowItDownController(MenuSearchService){
         }
         RefreshValidationMessage();
       });
-
       promise.catch(function (response){
         console.log(response);
+        RefreshValidationMessage();
       });
     }else{
       controller.found = [];
