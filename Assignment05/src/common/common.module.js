51e7f9d748d7997fd3052bc2066c337940fb0ad7
Vishwas Anand Bhushan <vishwasanand.cse@gmail.com>
2016-11-05 02:13:58 +0530

    https added to url

--- Assignment05/src/common/common.module.js
+++ Assignment05/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://vishwas1-angular.herokuapp.com')
+.constant('ApiPath', 'https://vishwas1-angular.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
